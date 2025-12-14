1872f536f77ed2e7abe72f493536bd1ab22f4eb4
DAKSHVS <dakshvs173@gmail.com>
2025-12-14 21:00:44 +0530

    Add files via upload
    first commit

---
+++ simple-app/src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Automated Jenkins Build!");
+        System.out.println("Triggered by GitHub push!");
+    }
+}
